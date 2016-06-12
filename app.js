fe3fe4a587663b8555983d3efdd5703a3f7c741d
Mitchell Slep <mitch@moomoobloo.com>
2016-06-11 22:54:51 -0700

    Enable CORS.

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ function create (env, ctx) {
   app.set('title', appInfo);
   app.enable('trust proxy'); // Allows req.secure test on heroku https connections.
 
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  };  
+  app.use(allowCrossDomain);
+    
   app.use(compression({filter: function shouldCompress(req, res) {
     //TODO: return false here if we find a condition where we don't want to compress
     // fallback to standard filter function
